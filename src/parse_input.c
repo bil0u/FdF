6b3024dd0083bada9a673832f13935cf8afe2d5b
upopee <upopee@student.42.fr>
2017-04-03 22:58:05 +0200

    Minor changes

--- src/parse_input.c
+++ src/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/01 09:49:36 by upopee            #+#    #+#             */
-/*   Updated: 2017/04/03 21:05:25 by upopee           ###   ########.fr       */
+/*   Updated: 2017/04/03 22:49:53 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "libft.h"
 #include "fdf.h"
 
-static void	end_error(t_list **lst, t_map **m, int i)
+static void	end_error(t_list **lst, t_map **m, int i, char *msg)
 {
 	if ((*m)->nb_lines != ERROR && (*m)->nb_columns != ERROR)
 	{
@@ -28,6 +28,7 @@ static void	end_error(t_list **lst, t_map **m, int i)
 	}
 	ft_memdel((void **)m);
 	ft_lstdel(lst, &ft_delcontent);
+	ft_putendl_fd(msg, 2);
 	exit(EXIT_FAILURE);
 }
 
@@ -59,15 +60,15 @@ static int	count_and_check(t_list *lst)
 
 	if (!lst)
 		return (ERROR);
-	ref_value = ft_nbwords(lst->content, ' ');
+	ref_value = ft_nbwords(lst->content, MAP_SEPARATOR);
 	while (lst)
 	{
 		str = lst->content;
-		if (ft_nbwords(str, ' ') != (size_t)ref_value)
+		if (ft_nbwords(str, MAP_SEPARATOR) != (size_t)ref_value)
 			return (ERROR);
 		while (*str)
 		{
-			if ((ret = ft_strisnumber(str, ' ')) == 0)
+			if ((ret = ft_strisnumber(str, MAP_SEPARATOR)) == 0)
 				return (ERROR);
 			str += str[ret] ? (ret + 1) : ret;
 		}
@@ -87,7 +88,7 @@ static int	*str_to_inttab(char *str, int nb_columns)
 	while (i < nb_columns)
 	{
 		line[i] = ft_atoi(str);
-		str = ft_strchr(str, ' ') + 1;
+		str = ft_strchr(str, MAP_SEPARATOR) + 1;
 		i++;
 	}
 	return (line);
@@ -103,19 +104,19 @@ t_map		*input_to_map(char *file)
 	lst = NULL;
 	i = 0;
 	if ((m = (t_map *)ft_memalloc(sizeof(*m))) == NULL)
-		end_error(&lst, &m, i);
+		end_error(&lst, &m, i, "malloc: cannot allocate memory");
 	if ((m->nb_lines = file_to_lst(file, &lst)) == ERROR)
-		end_error(&lst, &m, i);
+		end_error(&lst, &m, i, "open: cannot open file");
 	if ((m->nb_columns = count_and_check(lst)) == ERROR)
-		end_error(&lst, &m, i);
+		end_error(&lst, &m, i, "fdf: file not valid");
 	i = m->nb_lines;
 	if ((m->tab = (int **)ft_memalloc(sizeof(int *) * m->nb_lines)) == NULL)
-		end_error(&lst, &m, i);
+		end_error(&lst, &m, i, "malloc: cannot allocate memory");
 	leak = lst;
 	while (i-- > 0)
 	{
 		if ((m->tab[i] = str_to_inttab(lst->content, m->nb_columns)) == NULL)
-			end_error(&lst, &m, i);
+			end_error(&lst, &m, i, "malloc: cannot allocate memory");
 		lst = lst->next;
 	}
 	ft_lstdel(&leak, &ft_delcontent);
